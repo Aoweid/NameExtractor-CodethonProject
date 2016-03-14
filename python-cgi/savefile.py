d544645d5ccdb7ac3af011a2bc9a3960c93155b6
aronic <aronic@outlook.com>
2016-03-14 22:13:50 +0800

    change the deploy

--- python-cgi/savefile.py
+++ python-cgi/savefile.py
@@ -12,17 +12,32 @@ fileitem = form['filename']
 # 检测文件是否上传
 if fileitem.filename:
    # 设置文件路径
-   fn = os.path.basename(fileitem.filename)
-   open('../upload/' + fn, 'wb').write(fileitem.file.read())
-   message = 'The file "' + fn + '" was uploaded successfully'
+   filename = os.path.basename(fileitem.filename)
+   filepath='/tmp/'+filename
+   open(filepath, 'wb').write(fileitem.file.read())
+   message = 'The file "' + filepath + '" was uploaded successfully'
 else:
    message = 'No file was uploaded'
 
-print """\
-Content-Type: text/html\n
-<html>
-<body>
-   <p>%s</p>
-</body>
-</html>
-""" % (message,)
+
+# print """\
+# Content-Type: text/html\n
+# <html>
+# <body>
+#    <p>%s</p>
+# </body>
+# </html>
+# """ % (message,)
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 文件下载
+print """Content-Disposition: attachment; filename=\"%s\"\r\n\n"""%(filename);
+
+# 打开文件
+fo = open(filepath, "rb")
+str = fo.read();
+print str
+# 关闭文件
+fo.close()
