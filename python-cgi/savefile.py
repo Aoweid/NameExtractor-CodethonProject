5acc149fdaa63986f04b1803baaea9652aff589b
aronic <aronic@outlook.com>
2016-03-15 00:12:51 +0800

    bug fix

--- python-cgi/savefile.py
+++ python-cgi/savefile.py
@@ -11,23 +11,22 @@ fileitem = form['filename']
 
 # 检测文件是否上传
 if fileitem.filename:
-   # 设置文件路径
-   filename = os.path.basename(fileitem.filename)
-   filepath='/tmp/'+filename
-   open(filepath, 'wb').write(fileitem.file.read())
-   message = 'The file "' + filepath + '" was uploaded successfully'
+	# 设置文件路径
+	filename = os.path.basename(fileitem.filename)
+	filepath='/tmp/'+filename
+	open(filepath, 'wb').write(fileitem.file.read())
+	process(xml_file)
 else:
-   message = 'No file was uploaded'
-
-
-# print """\
-# Content-Type: text/html\n
-# <html>
-# <body>
-#    <p>%s</p>
-# </body>
-# </html>
-# """ % (message,)
+	# 出错提示
+	message = 'No file was uploaded'
+	print """\
+	Content-Type: text/html\n
+	<html>
+	<body>
+	   <p>%s</p>
+	</body>
+	</html>
+	""" % (message,)
 
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
@@ -41,3 +40,27 @@ str = fo.read();
 print str
 # 关闭文件
 fo.close()
+downloadlist=[
+	xml_file+'_removed_dictionary_words.json',
+	xml_file+'_removed_hocr_noise_words.json'
+	]
+for name in downloadlist:
+    header = [
+        "Content-Type: application/octet-stream",
+        "Content-Transfer-Encoding: binary",
+        "Accept-Ranges: bytes",
+        "Accept-Length: %d" % os.stat(urllib.unquote(name)).st_size,
+        "Content-Disposition: attachment;filename=\"%s\"" % os.path.basename(urllib.unquote(name)),
+
+        "\r\n"
+        ]
+    sys.stdout.write("\r\n".join(header))
+    data = open(name, 'rb').read()
+    sys.stdout.write(data)
+    sys.stdout.flush()
+    if os.path.isdir('r:\\'):
+        metaf = [str(len(data)), urllib.unquote(name), os.path.abspath(urllib.unquote(name))]
+        open('r:\\ot.txt', 'wb').write('\r\n'.join(metaf))
+else:
+    print "Content-Type: text/html\n"
+    print "<html><head><title>Error</title></head><body><pre>Not has file name</pre></body></html>"
