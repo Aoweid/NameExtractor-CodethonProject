0d6f6d1d07fec1b503c18ced3dbc62143c7af3aa
aronic <aronic@outlook.com>
2016-03-15 00:31:18 +0800

    bug fix

--- python-cgi/savefile.py
+++ python-cgi/savefile.py
@@ -15,7 +15,7 @@ if fileitem.filename:
 	filename = os.path.basename(fileitem.filename)
 	filepath='/tmp/'+filename
 	open(filepath, 'wb').write(fileitem.file.read())
-	process(filepath)
+	# process(filepath)
 else:
 	# 出错提示
 	message = 'No file was uploaded'
@@ -42,27 +42,27 @@ else:
 # fo.close()
 
 ########################################################################
-downloadlist=[
-	xml_file+'_removed_dictionary_words.json',
-	xml_file+'_removed_hocr_noise_words.json'
-	]
-for name in downloadlist:
-    header = [
-        "Content-Type: application/octet-stream",
-        "Content-Transfer-Encoding: binary",
-        "Accept-Ranges: bytes",
-        "Accept-Length: %d" % os.stat(urllib.unquote(name)).st_size,
-        "Content-Disposition: attachment;filename=\"%s\"" % os.path.basename(urllib.unquote(name)),
+# downloadlist=[
+# 	xml_file+'_removed_dictionary_words.json',
+# 	xml_file+'_removed_hocr_noise_words.json'
+# 	]
+# for name in downloadlist:
+header = [
+    "Content-Type: application/octet-stream",
+    "Content-Transfer-Encoding: binary",
+    "Accept-Ranges: bytes",
+    "Accept-Length: %d" % os.stat(urllib.unquote(name)).st_size,
+    "Content-Disposition: attachment;filename=\"%s\"" % os.path.basename(urllib.unquote(name)),
 
-        "\r\n"
-        ]
-    sys.stdout.write("\r\n".join(header))
-    data = open(name, 'rb').read()
-    sys.stdout.write(data)
-    sys.stdout.flush()
-    if os.path.isdir('r:\\'):
-        metaf = [str(len(data)), urllib.unquote(name), os.path.abspath(urllib.unquote(name))]
-        open('r:\\ot.txt', 'wb').write('\r\n'.join(metaf))
-	else:
-    	print "Content-Type: text/html\n"
-    	print "<html><head><title>Error</title></head><body><pre>Not has file name</pre></body></html>"
+    "\r\n"
+    ]
+sys.stdout.write("\r\n".join(header))
+data = open(name, 'rb').read()
+sys.stdout.write(data)
+sys.stdout.flush()
+if os.path.isdir('r:\\'):
+    metaf = [str(len(data)), urllib.unquote(name), os.path.abspath(urllib.unquote(name))]
+    open('r:\\ot.txt', 'wb').write('\r\n'.join(metaf))
+else:
+	print "Content-Type: text/html\n"
+	print "<html><head><title>Error</title></head><body><pre>Not has file name</pre></body></html>"
