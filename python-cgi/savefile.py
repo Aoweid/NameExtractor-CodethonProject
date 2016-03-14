1216d0d99e8a9105921081b545970c0896d2e204
Alvin <aoweid@seas.upenn.edu>
2016-03-14 17:01:24 -0400

    Update savefile.py

--- python-cgi/savefile.py
+++ python-cgi/savefile.py
@@ -106,7 +106,7 @@ else:
 command='cd /tmp;'
 command+= 'tar -zcvf result.tar.gz '+filename.replace('.xml', '_name.json')+' '+ filename.replace('.xml', '_removed_dictionary_words.json')+' ' +filename.replace('.xml', '_removed_hocr_noise_words.json')+';'
 command+='cd /var/www/cgi-bin'
-tmp = open('tmp.txt','w')
+tmp = open('/tmp/tmp.txt','w')
 tmp.write(command)
 tmp.close()
 a=os.system(command)#不要删除变量a, 这是为了抑制输出, 因为这些输出会被当做应答
